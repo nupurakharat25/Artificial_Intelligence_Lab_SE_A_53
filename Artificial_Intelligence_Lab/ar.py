c604205c0e6491fe505ec3d93a99e2188358bed0
nupurakharat25 <nupura.kharat25@mmit.edu.in>
2026-07-24 12:02:55 +0530

    files created and deleted

---
+++ Artificial_Intelligence_Lab/ar.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("\n--- Arithmetic Results ---")
+print(f"Addition ({num1} + {num2}) = {num1 + num2}")
+print(f"Subtraction ({num1} - {num2}) = {num1 - num2}")
+print(f"Multiplication ({num1} * {num2}) = {num1 * num2}")
